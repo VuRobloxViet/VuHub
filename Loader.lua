c642f8d2f4ceb2c6e879efe943b02269bc6d97b9
VuRobloxViet <phinguyen.nt101179@gmail.com>
2024-09-05 14:57:38 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,13 +1,13 @@
 if game.PlaceId == 10464237910 then
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxVietNam/VuHub/main/DragonSeaFree.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxVietNam/VuHub/main/DragonSeaFree.lua"))()
 elseif game.PlaceId == 15697104413 then
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxVietNam/VuHub/main/DragonSeaFree.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxVietNam/VuHub/main/DragonSeaFree.lua"))()
 elseif game.PlaceId == 13373351184 then
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxVietNam/VuHub/main/DragonSeaFree.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxVietNam/VuHub/main/DragonSeaFree.lua"))()
 elseif game.PlaceId == 5811374318 then
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxVietNam/VuHub/main/DragonPiece.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxVietNam/VuHub/main/DragonPiece.lua"))()
 elseif game.PlaceId == 12048634049 then
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxVietNam/VuHub/main/DragonPiece.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxVietNam/VuHub/main/DragonPiece.lua"))()
 else
-      game.Players.LocalPlayer:Kick("Script Not Support For The Game. Please Check Script Support Game or Script Status For The Game In Discord.")
+    game.Players.LocalPlayer:Kick("Script Not Support For The Game. Please Check Script Support Game or Script Status For The Game In Discord.")
 end
