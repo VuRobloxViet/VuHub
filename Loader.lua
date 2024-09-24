daef3715b891c60fc9b53529538d3517c98a8c58
VuRobloxViet <phinguyen.nt101179@gmail.com>
2024-09-24 20:58:56 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -8,6 +8,4 @@ elseif game.PlaceId == 5811374318 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxViet/VuHub/main/DragonPiece.lua"))()
 elseif game.PlaceId == 12048634049 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxViet/VuHub/main/DragonPiece.lua"))()
-else
-    game.Players.LocalPlayer:Kick("Script Not Support For The Game. Please Check Script Support Game or Script Status For The Game In Discord.")
 end
