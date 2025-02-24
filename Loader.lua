9226fa25302b24a4700f59992b501533bcfc70b9
VuRobloxViet <phinguyen.nt101179@gmail.com>
2025-02-24 19:48:08 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 10464237910 or game.PlaceId == 15697104413 or game.PlaceId == 13373351184 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxViet/VuHub/refs/heads/main/DragonSeaFree.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxViet/VuHub/refs/heads/main/DragonSea.lua"))()
 elseif game.PlaceId == 5811374318 or game.PlaceId == 12048634049 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxViet/VuHub/refs/heads/main/DragonPiece.lua"))()
 elseif game.PlaceId == 10449761463 then
