0b6618aee4e4e913084184930d5792f07c694868
VuRobloxViet <phinguyen.nt101179@gmail.com>
2025-02-27 21:06:08 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,5 @@
 if game.PlaceId == 10464237910 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxViet/VuHub/refs/heads/main/DragonSea.lua"))()
-elseif game.PlaceId == 5811374318 or game.PlaceId == 12048634049 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxViet/VuHub/refs/heads/main/DragonPiece.lua"))()
 elseif game.PlaceId == 10449761463 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxViet/VuHub/refs/heads/main/TheStrongestBattlegrounds.lua"))()
 end
