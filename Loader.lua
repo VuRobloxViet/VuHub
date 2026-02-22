f38367c8b6d34701ec4e41a891e6fec206867e92
VuRobloxViet <phinguyen.nt101179@gmail.com>
2026-02-22 11:53:39 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,8 @@
+local VirtualUser = game:GetService("VirtualUser")
+game.Players.LocalPlayer.Idled:Connect(function()
+    VirtualUser:CaptureController()
+    VirtualUser:ClickButton2(Vector2.new())
+end)
 if game.PlaceId == 131623223084840 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VuRobloxViet/VuHub/main/EscapeTsunamiForBrainrot.lua"))()
 end
